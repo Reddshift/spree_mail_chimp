bd0f95625a96b6a7c0bc540fb8836ffcee169cfd
Drew <andrewmp1@gmail.com>
2011-06-01 10:18:13 -0700

    Edited config/routes.rb via GitHub

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :subscriptions, :only => :create
+  resources :newsletter_subscriptions, :only => :create
 
   namespace :admin do
       resource  :mail_chimp_settings
